83ea9024641a861b5035ceff2b6675d2770e0ebd
Alex Berg <chexxor@gmail.com>
2019-01-23 21:01:18 -0600

    Move mouse and arrows.

--- keyboards/7skb/keymaps/chexxor/keymap.c
+++ keyboards/7skb/keymaps/chexxor/keymap.c
@@ -22,21 +22,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT,          KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,             KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LALT, KC_LGUI, KC_LALT, KC_FN1,  KC_LCTL,          KC_SPFN1,                  KC_FN2,  KC_RALT, KC_RGUI, KC_RCTL
+    KC_LALT, KC_LGUI, KC_LALT, KC_FN1,  KC_LCTL,          KC_SPFN1,                  KC_FN2,  KC_RALT, KC_RGUI, KC_BTN1
   ),
 
   [_FN1] = LAYOUT(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-    _______, KC_VOLU, _______, KC_LBRC, KC_RBRC, _______, _______, KC_4,    KC_5,    KC_6,    KC_SCLN,          _______,
-    _______, KC_VOLD, _______, KC_LCBR, KC_RCBR, _______, _______,          KC_1,    KC_2,    KC_3,    KC_UP,   _______,
-    _______, _______, _______, _______, _______,          KC_DEL,                    KC_0,    KC_LEFT, KC_DOWN, KC_RGHT
+    _______, KC_MPLY, KC_VOLU, KC_MNXT, _______, _______, _______, KC_4,    KC_5,    KC_6,    KC_SCLN,          _______,
+    _______, _______, KC_VOLD, KC_MPRV, _______, _______, _______,          KC_1,    KC_2,    KC_3,    KC_BSLS, _______,
+    _______, _______, _______, _______, _______,          KC_DEL,                    KC_0,    _______, _______, _______
   ),
 
   [_FN2] = LAYOUT(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS,
-    _______, _______, _______, KC_INS,  KC_PGUP, KC_HOME, _______, _______, _______, _______, _______,          _______,
-    _______, _______, _______, KC_DEL,  KC_PGDN, KC_END,  _______,          _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______,           _______,                   _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_UP,   KC_PGDN, KC_HOME, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,           _______,
+    _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,          _______,                   _______, _______, _______, _______
   )
 
 };
